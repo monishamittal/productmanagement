aea77b0ed66b6c570e20eb633c59bc476105c756
monishamittal <monishamittal0673@gamil.com>
2022-07-25 17:52:51 +0530

    1

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,73 +1,52 @@
 //.................................... Import Models for using in this module ....................//
 const mongoose = require("mongoose");
-// const ObjectId = mongoose.Schema.Types.ObjectId;
+
 
 //................................. Create Schema .........................//
-const userSchema = new mongoose.Schema(
-    {
-        fname: {
-            type: String,
-            required: true
-        },
-        lname: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true,
-            // valid email, 
-            unique: true
-        },
-        profileImage: {
-            type: String,
-            required: true
-        }, // s3 link
-        phone: {
-            type: String,
-            required: true,
-            unique: true,
-            //  valid Indian mobile number
-        },
-        password: {
-            type: String,
-            required: true,
-            //  minLen 8, maxLen 15
-        }, // encrypted password
-        address: {
-            shipping: {
-                street: {
-                    type: String,
-                    required: true
-                },
-                city: {
-                    type: String,
-                    required: true
-                },
-                pincode: {
-                    type: Number,
-                    required: true
-                }
-            }
-        },
-        billing: {
-            street: {
-                type: String,
-                required: true
-            },
-            city: {
-                type: String,
-                required: true
-            },
-            pincode: {
-                type: Number,
-                required: true
-            }
-        }
-
+const userSchema = new mongoose.Schema({
+    fname: {
+        type:String, 
+        required:true
     },
-    { timestamps: true },
-);
+    lname: {
+        type:String, 
+        required:true
+    },
+    email: {
+        type:String, 
+        required:true, 
+        valid:true, 
+        unique:true},
+    profileImage: {
+        type:String, 
+        required:true
+    },                          // s3 link
+    phone: {
+        type:String, 
+        required:true, 
+        unique:true, 
+        valid:true 
+    }, 
+    password: {
+        type:String, 
+        required:true
+    },                   // encrypted password
+    address: {
+      shipping: {
+        street: {type:String, required:true},
+        city: {type:String, required:true},
+        pincode: {type:Number, required:true}
+      },
+      billing: {
+        street: {type:String, required:true},
+        city: {type:String, required:true},
+        pincode: {type:Number, required:true}
+      },
+    },
+    
+  
 
-//........................................Export Schema..................................//
+    },{ timestamps: true });
+
+
 module.exports = mongoose.model("User", userSchema);                         //provides an interface to the database like CRUD operation
